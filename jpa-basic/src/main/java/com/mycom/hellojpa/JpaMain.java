7bee64f99a93e9e8fc289551700c1be85904e673
dayoon97 <ekdbsekdbs@gmail.com>
2021-02-19 17:18:00 +0900

    영속성 컨텍스트
    영속과 비영속 비교

--- jpa-basic/src/main/java/com/mycom/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/com/mycom/hellojpa/JpaMain.java
@@ -19,19 +19,16 @@ public class JpaMain {
 		tx.begin();
 		
 		try {
-//			Member findMember = em.find(Member.class, 1L);
+
+			//비영속
+			Member member = new Member();
+			member.setId(100L);
+			member.setName("HelloJPA");
 			
-			List<Member> result = em.createQuery("select m from Member as m", Member.class)
-					//조회 시작위치(0부터 시작)
-					.setFirstResult(5)
-					//조회할 데이터 수
-					.setMaxResults(8)
-					//결과를 리스트로 받는다.(결과가 없으면 빈 컬렉션을 반환.)
-					.getResultList();
-			
-			for (Member member: result) {
-				System.out.println("member.name = " + member.getName());
-			}
+			//영속
+			System.out.println(" === BEFORE ===");
+			em.persist(member);
+			System.out.println(" === AFTER === ");
 			
 			tx.commit();
 		} catch (Exception e) {
